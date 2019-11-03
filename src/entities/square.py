5874766d01be78d47e59692d5f1b35b5a2c96c5c
Mateusz Palkowski <mateusz.palkowski@gmail.com>
2019-11-03 22:28:06 +0100

    Added crossover and mutation

--- src/entities/square.py
+++ src/entities/square.py
@@ -1,19 +1,50 @@
 from PIL import Image, ImageDraw
 import random
+import math
+import numpy as np
 
-
+# Zmienne:
+# kolor + alpha
+# pozycja
+# wymiary
 class Square:
 
-    def __init__(self, border_x, border_y):
-        self.color = [random.randint(0, 255) for k in range(3)]
-        self.alpha = random.randint(0, 100)
-        self.RGBA_color = tuple([*self.color, self.alpha])
-        self.point = (random.randint(0, border_x), random.randint(0, border_y))   #TODO: CHECK!
-        self.dim = [random.randint(0, border_x-self.point[0]), random.randint(0, border_y-self.point[1])]   #TODO: CHECK!
+    def __init__(self, border_x=0, border_y=0):
+        if border_x == 0 and border_y == 0:
+            self.color = []
+            self.alpha = 0
+            self.RGBA_color = tuple([*self.color, self.alpha])
+            self.point = (0, 0)  # TODO: CHECK!
+            self.dim = [0, 0]  # TODO: CHECK!
+        else:
+            self.color = [random.randint(0, 255) for k in range(3)]
+            self.alpha = random.randint(0, 100)
+            self.RGBA_color = tuple([*self.color, self.alpha])
+            self.point = (random.randint(0, border_x), random.randint(0, border_y))   #TODO: CHECK!
+            self.dim = [random.randint(0, border_x-self.point[0]), random.randint(0, border_y-self.point[1])]   #TODO: CHECK!
 
     def draw(self, image):
         drw = ImageDraw.Draw(image, 'RGBA')
         drw.polygon([self.point,  (self.point[0], self.point[1] + self.dim[1]),
                      (self.point[0] + self.dim[0], self.point[1] + self.dim[1]),
                      (self.point[0] + self.dim[0], self.point[1])], fill=self.RGBA_color)
+
+    def mutate_square(self, sigmas, n):
+        for i in range(0, 3):
+            self.color[i] = self.color[i] + sigmas.color_sigma[i] * n
+        self.alpha = self.alpha + sigmas.alpha_sigma * n
+        self.point = (self.point[0] + sigmas.point_sigma[0] * n, self.point[1] + sigmas.point_sigma[1] * n)
+        self.dim = [self.dim[0] + sigmas.dim_sigma[0] * n, self.dim[1] + sigmas.dim_sigma[1] * n]
+
+
+# uśrednij dwa kwadraty
+def average_square(s1, s2):
+    s = Square()
+    for i in range(0, len(s1.color)):
+        s.color.append(((s1.color[i] + s2.color[i])//2))
+    s.alpha = (s1.alpha + s2.alpha)//2
+    s.RGBA_color = tuple([*s.color, s.alpha])
+    s.point = ((s1.point[0] + s2.point[0])/2, (s1.point[1] + s2.point[1])//2)
+    s.dim = [(s1.dim[0] + s2.dim[0])/2, (s1.dim[1] + s2.dim[1])//2]
+    return s
 
--- src/test.py
+++ src/test.py
@@ -1,8 +1,11 @@
 from PIL import Image
 import copy
-
+import random
 import numpy as np
+import math
 from PIL import Image, ImageDraw
+from src.entities.square import Square
+from src.entities.square import average_square
 
 # TODO: Mateusz: Krzyżowanie, mutacja,
 #       Michał: Funkcja celu, kwadraty + funkcja konwersji
@@ -12,27 +15,110 @@ from PIL import Image, ImageDraw
 
 
 class Population:
-    def __init__(self, miu, square_num, image_x, image_y):
+    def __init__(self, child=False, miu=0, square_num=0, image_x=0, image_y=0):
         # Generate population
         print('Generating population of size:', miu)
-        specimen = []
-        for i in range(0, miu):
-            specimen.append(Specimen(square_num, image_x, image_y))
+        self.specimen = []
+        if not child:
+            for i in range(0, miu):
+                self.specimen.append(Specimen(square_num, image_x, image_y))
+
+    def show_specimen(self):
+        for s in self.specimen:
+            s.get_image().show()
 
 
+def generate_random(population, l):
+    temporary = Population(True)
+    for i in range(l):
+        temporary.specimen.append(random.choice(population.specimen))
+    return temporary
+
+def crossover(temp):
+    # Dobieraj osobniki w pary 1 z 2, 2 z 3, 3 z 1
+    reproduced = Population(True)
+    for i in range(0, len(temp.specimen)):
+        if i == len(temp.specimen) - 1:
+            s1 = temp.specimen[i]
+            s2 = temp.specimen[0]
+        else:
+            s1 = temp.specimen[i]
+            s2 = temp.specimen[i+1]
+        s = Specimen(s1.square_num, s1.image_x, s1.image_y, True)
+
+        for j in range(0, s1.square_num):
+            s.squares.append(average_square(s1.squares[j], s2.squares[j]))
+            s.sigmas.append(average_sigma(s1.sigmas[j], s2.sigmas[j]))
+
+        reproduced.specimen.append(s)
+    return reproduced
+
+
+def mutation(reproduced, tau, tau_prim):
+    for spec in reproduced.specimen:
+        n = np.random.normal(0, 1, len(spec.squares))
+        n1 = np.random.normal(0, 1, len(spec.squares))
+        n2 = np.random.normal(0, 1, len(spec.squares))
+        for i in range(0, len(spec.squares)):
+            spec.sigmas[i].mutate_sigmas(tau, tau_prim, n1[i], n2[i])
+            spec.squares[i].mutate_square(spec.sigmas[i], n[i])
+    return reproduced
+
+class Sigma:
+    def __init__(self, square=None):
+        self.a = 0.1
+        if square is None:
+            self.color_sigma = []
+            self.alpha_sigma = 0
+            self.point_sigma = (0, 0)
+            self.dim_sigma = [0, 0]
+        else:
+
+            self.color_sigma = [square.color[0] * self.a, square.color[1] * self.a, square.color[2] * self.a]
+            self.alpha_sigma = self.a * square.alpha
+            self.point_sigma = (square.point[0] * self.a, square.point[1] * self.a)
+            self.dim_sigma = [square.dim[0] * self.a, square.point[1] * self.a]
+
+    def mutate_sigmas(self, tau, tau_prim, n1, n2):
+        for i in range(0,3):
+            self.color_sigma[i] = self.color_sigma[i] * math.exp(tau * n1 + tau_prim * n2)
+        self.alpha_sigma = self.alpha_sigma * math.exp(tau * n1 + tau_prim * n2)
+        self.point_sigma = (self.point_sigma[0] * math.exp(tau * n1 + tau_prim * n2),
+                            self.point_sigma[1] * math.exp(tau * n1 + tau_prim * n2))
+        self.dim_sigma = [self.dim_sigma[0] * math.exp(tau * n1 + tau_prim * n2),
+                          self.dim_sigma[1] * math.exp(tau * n1 + tau_prim * n2)]
+
+
+def average_sigma(s1, s2):
+    s = Sigma()
+    for i in range(0, len(s1.color_sigma)):
+        s.color_sigma.append(((s1.color_sigma[i] + s2.color_sigma[i]) // 2))
+    s.alpha_sigma = (s1.alpha_sigma + s2.alpha_sigma) // 2
+    s.point_sigma = ((s1.point_sigma[0] + s2.point_sigma[0]) / 2, (s1.point_sigma[1] + s2.point_sigma[1]) // 2)
+    s.dim_sigma = [(s1.dim_sigma[0] + s2.dim_sigma[0]) / 2, (s1.dim_sigma[1] + s2.dim_sigma[1]) // 2]
+    return s
 # Member of a population
 class Specimen:
-    def __init__(self, square_num, image_x, image_y):
+    def __init__(self, square_num, image_x, image_y, child=False):
         # Generate one member of a population - specimen(image)
+        self.image_x = image_x
+        self.image_y = image_y
         self.squares = []
-        self.sigmas = np.zeros(square_num);
-        for i in range(square_num):
-            self.squares.append(Square(100, 100))
-            self.rgbmap = np.zeros((image_x, image_y, 3), dtype=np.uint8) + 255
-            print(self.rgbmap.shape)
-            #self.image = Image.fromarray(self.rgbmap)
-            #self.image.show()
+        self.square_num = square_num
+        self.image = Image.new('RGB', (image_x, image_y), (255, 255, 255))
+        self.sigmas = []
 
+        if not child:
+            for i in range(square_num):
+                self.squares.append(Square(image_x, image_y))
+                self.sigmas.append(Sigma((self.squares[i])))
+
+
+    def get_image(self):
+        background = Image.new('RGB', (self.image_x, self.image_y), (255, 255, 255))
+        for s in self.squares:
+            s.draw(background)
+        return background
 
 
 # Check if algorithm should stop
@@ -60,27 +146,41 @@ def rgba_to_rgb(rgba_map):
 
 def main():
     print("starting algorithm")
-    u = 10  # miu
-    l = 12  # lambda
-    prostokaty=100
-    image_x=100
-    image_y=100
-    population = Population(u, prostokaty, image_x, image_y)
+    u = 1  # miu
+    l = 1  # lambda
 
-    # Algorith
-    while not is_stop(population):
-        # TODO: 1. wybrac populacje o dobrej wielkosci - funkcja generate_random()
-        #       2.
-        #
-        # Create temporary population T
-        temporary_population = population.generate_random(l)
+    prostokaty = 50
+    tau = 1/ (math.sqrt(prostokaty*2))
+    tau_prim = 1/ (math.sqrt(2* math.sqrt(prostokaty)))
+    image_x = 1000
+    image_y = 1000
+    population = Population(False, u, prostokaty, image_x, image_y)
 
-        # Selection: crossover, mutation then selection
-        reproduced = temporary_population.selection()
-        population = reproduced
-    else:
-        # If the stop condition is met, return best specimen
-        best = population.get_best()
+    temporary = generate_random(population, l)
+    temporary.show_specimen()
+    for i in range(100):
+        reproduced = crossover(temporary)
+        reproduced = mutation(reproduced, tau, tau_prim)
+    reproduced.show_specimen()
+
+    # Algorithm
+
+
+# while not is_stop(population):
+# TODO: 1. wybrac populacje o dobrej wielkosci - funkcja generate_random() - done
+#       2. Utworzenie potomnej populacji:
+#             2.1 Krzyżowanie - done, wersja 1. czyli usrednienie rodzicow
+#             2.2 Mutacja
+#       3. Wybór nowej populacji
+# Create temporary population T
+# temporary_population = generate_random(population, l)
+
+# Selection: crossover, mutation then selection
+# reproduced = temporary_population.selection()
+# population = reproduced
+# else:
+# If the stop condition is met, return best specimen
+# best = population.get_best()
 
 if __name__ == '__main__':
     main()
